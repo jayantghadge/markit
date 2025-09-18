e77dd7bac68a12c0d9e741edf18ab07bf865d417
Jayant Ghadge <jayantghadge6@gmail.com>
2025-09-18 20:49:16 +0530

    initial commit

---
+++ src/main/java/com/app/markit/MarkitApplication.java
@@ -0,0 +1,13 @@
+package com.app.markit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/markit/MarkitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.markit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
