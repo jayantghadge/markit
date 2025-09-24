2a4b8adb7e21c47ba625bf54e2897324773b2f15
Jayant Ghadge <jayantghadge6@gmail.com>
2025-09-24 23:48:14 +0530

    add toggle and delete functionality for tasks

--- src/main/java/com/app/markit/controller/TaskController.java
+++ src/main/java/com/app/markit/controller/TaskController.java
@@ -6,10 +6,7 @@ import com.app.markit.models.Task;
 import com.app.markit.services.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,4 +33,17 @@ public class TaskController {
         taskService.createTask(title);
         return "redirect:/";
     }
+
+    @GetMapping("{id}/delete")
+    public String deleteTasks(@PathVariable Long id){
+        taskService.deleteTask(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("{id}/toggle")
+    public String toggleTasks(@PathVariable Long id){
+        taskService.toggleTask(id);
+        return "redirect:/";
+    }
+
 }
